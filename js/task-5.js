3b7639c8ccc0944b72646439be0e8d6842320d0f
Pavlo-Chernicenko <pwned7067@gmail.com>
2024-09-01 10:40:48 +0300

    Added finished 5,6  tasks

--- js/task-5.js
+++ js/task-5.js
@@ -1,6 +1,15 @@
 'use strict';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+
+btn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.querySelector('.color').textContent = newColor;
+  document.querySelector('body').style.backgroundColor = newColor;
+});
--- js/task-6.js
+++ js/task-6.js
@@ -1,6 +1,36 @@
 'use strict';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const startSizeBox = 30;
+
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const boxArea = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', () => {
+  boxArea.innerHTML = '';
+  const quantity = parseInt(document.querySelector('[type="number"]').value);
+  const boxsMarkup = [];
+
+  if (0 < quantity && quantity < 101) {
+    for (let index = 0; index < quantity; index++) {
+      const size = startSizeBox + index * 10;
+      boxsMarkup.push(
+        `<div style="width: ${size}px; 
+        height: ${size}px; 
+        background-color: ${getRandomHexColor()};"></div>`
+      );
+    }
+
+    boxArea.innerHTML = boxsMarkup.join('');
+  }
+});
+
+btnDestroy.addEventListener('click', () => {
+  boxArea.innerHTML = '';
+});
